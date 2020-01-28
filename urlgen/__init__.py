3316aef75419ac0bdfa4970b40bab1c2ab8d7ea6
kazukazuprogram <dbycvil8yiyf7xnlxvh7@yahoo.co.jp>
2020-01-28 14:06:55 +0900

    A

--- urlgen/__init__.py
+++ urlgen/__init__.py
@@ -24,6 +24,12 @@ def get(url):
     elif dom == "uploadhaven.com":
         # print("UPLOADHAVEN")
         return uploadhaven(url=url)
+    elif dom == "drive.google.com":
+        # print("GDRIVE")
+        return gdrive(url=url)
+    elif dom.split(".")[1:] == ["zippyshare", "com"]:
+        # print("ZippyShare")
+        return zippyshare(url=url)
     elif dom == "www.mediafire.com" or dom == "mediafire.com":
         # print("MediaFire")
         return mediafire(url=url)
@@ -35,7 +41,8 @@ def get(url):
 def uploadhaven(url, s=Session()):
     t = s.get(url, proxies=global_proxy).text
     f = bs(t, "lxml")
-    size = f.find("td", class_="responsiveInfoTable").text.split("\n")[2].strip()
+    # size = f.find("td",
+    #               class_="responsiveInfoTable").text.split("\n")[2].strip()
     # print(size)
     f = f.find("form", class_="contactForm")
     postdata = {
@@ -54,36 +61,57 @@ def uploadhaven(url, s=Session()):
 
 
 def megaup(url, s=Session()):
-    # print("URL :", url)
     f = bs(s.get(url, proxies=global_proxy).text, "lxml")
-    clink = f.find("div", class_="row").script.text.split("href='")[1].split("'")[0]
-    size = f.find("td", class_="responsiveInfoTable").text.split("\n")[2].strip()
-    # print(size)
+    clink = f.find("div",
+                   class_="row").script.text.split("href='")[1].split("'")[0]
+    size = f.find("td", class_="responsiveInfoTable").text.split(
+        "\n")[2].strip()
     for x in range(6, 0, -1):
-        # print("\r"+str(x), end="")
         sleep(1)
-    # print()
-    # print(clink)
-    # print("GO!")
     g = s.get(clink, proxies=global_proxy, allow_redirects=False)
-    # print("OK")
     return g.headers["location"]
 
 
 def mediafire(url, s=Session()):
     f = bs(s.get(url, proxies=global_proxy).text, "lxml")
-    f = f.find("div", id="download_link", class_="download_link").find("a", class_="input")
-    size = f.text.split()[1][1:-1]
+    f = f.find("div", id="download_link",
+               class_="download_link").find("a", class_="input")
+    # size = f.text.split()[1][1:-1]
     link = f.get("href")
     # print("Size :", size)
     return link
 
 
+def gdrive(url, s=Session()):
+    p = urlparse(url).path.split("/")[1:]
+    if p[0:2] == ["file", "d"]:
+        id = p[2]
+    elif p[-1] == "uc":
+        id = urlparse(url).query.split("id=")[1].split("&")[0]
+    url = "https://drive.google.com/uc?id={}&export=download".format(id)
+    g = s.get(url)
+    f = bs(g.text, "lxml")
+    url = "https://drive.google.com" + \
+        f.find("a", id="uc-download-link").get("href")
+    o = s.head(url, allow_redirects=True)
+    return o.url
+
+
+def zippyshare(url, s=Session()):
+    g = s.get(url)
+    f = bs(g.text, "lxml")
+    url = "https://" + urlparse(url).netloc + \
+        f.find("a", id="dlbutton").get("href")
+    return url
+
+
 if __name__ == '__main__':
     try:
-        url = get(argv[-1])
-        # print()
+        if len(argv) >= 2:
+            url = argv[1]
+        else:
+            url = input()
+        url = get(url)
         print(url)
     except Exception as e:
-        pass
-        # print("Error :", e)
+        print("Error :", e)
